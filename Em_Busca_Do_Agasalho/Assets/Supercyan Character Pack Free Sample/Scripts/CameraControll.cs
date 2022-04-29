2d0aa048ff904661aeb13a524074ff9f08357f0c
gabriel mace <gabrielmace1@hotmail.com>
2022-04-29 16:57:49 -0300

    Resolvido o problema da camera do personagem, agora é necessário reduzir a rotação da camera

---
+++ Em_Busca_Do_Agasalho/Assets/Supercyan Character Pack Free Sample/Scripts/CameraControll.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControll : MonoBehaviour
+{
+    // public Transform Player; // Player a ser seguido
+    // public float YOffset; // Ajuste a altura da camera
+    // // [Range(0, 500)]
+    // public float Sensibility; // Sens da camera
+    // // [Range(0, 360)]
+    // public float limiteRotation; // MIN E MAX
+
+    // float rotX;
+    // float rotY;
+
+    // void start() {
+
+    // }
+
+    // void Update() {
+    //     float Mouse_X = Input.GetAxis("Mouse Y");
+    //     float Mouse_Y = Input.GetAxis("Mouse X");
+
+    //     rotX -= Mouse_X * Sensibility * Time.deltaTime;
+    //     rotY += Mouse_Y * Sensibility * Time.deltaTime;
+
+    //     rotX = Mathf.clamp(rotX, -LimitRotation, LimitRotation);
+
+    //     transform.rotation = Quaternion.Euler(rotX, rotY, 0);
+
+    // }
+}
